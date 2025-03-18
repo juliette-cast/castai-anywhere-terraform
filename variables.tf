8bb611d29f985ad6f0d66fc84795572fd6959cc0
juliette-cast <juliette@cast.ai>
2025-03-18 12:08:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,25 @@
 variable "cast_ai_api_key" {
   description = "Your CAST AI API key"
   type        = string
-  # For demonstration, we set a default. In production, pass this as a variable instead.
-  default     = "cece9b3a81982494ada68b6f658b509f2010a1e147552c522def15b768f0aab0"
+  sensitive   = true
+  default     = "" # add your api key
 }
 
 variable "cluster_name" {
   description = "Name of your cluster"
   type        = string
-  default     = "minikube-cluster"
+  default     = "" # give a name for your cluster
 }
 
-# variable "cluster_id" {
-#   description = "Identifier for your anywhere cluster (used as clusterName and clusterID)"
-#   type        = string
-#   default     = "794f9c72-1024-4127-87e3-3fa07179a287"
-# }
+variable "cluster_id" {
+  description = "Identifier for your CAST AI cluster"
+  type        = string
+  default     = "" # add the cluster ID you copied from the UI from the 1st apply
+}
+
+variable "organization_id" {
+  description = "Your CAST AI Organization ID"
+  type        = string
+  default     = "" # add your Org ID ..this is required for pod mutator deployment
+}
 
